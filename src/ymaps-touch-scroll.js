d3c034500bf458af5a54bc75dd15f563fc2c198b
whooehoo <whooehoo@yandex.ru>
2019-08-04 14:48:44 +0300

    :bug: Fix drag-behavior bag :recycle: Refactoring lib

--- src/ymaps-touch-scroll.js
+++ src/ymaps-touch-scroll.js
@@ -1,103 +1,98 @@
-export default function ymapsTouchScroll(map, {
-  preventScroll = true,
-  preventTouch = true,
-  textScroll = 'Чтобы изменить масштаб, прокручивайте карту, удерживая клавишу Ctrl',
-  textTouch = 'Чтобы переместить карту проведите по ней двумя пальцами'
-} = {}) {
-  if ((typeof map !== 'object') || (!preventScroll && !preventTouch) || (typeof textScroll !== 'string') || (typeof textTouch !== 'string')) return;
+const ymapsTouchScroll = (
+  map,
+  {
+    preventScroll = true,
+    preventTouch = true,
+    textScroll = "Чтобы изменить масштаб, прокручивайте карту, удерживая клавишу Ctrl",
+    textTouch = "Чтобы переместить карту проведите по ней двумя пальцами"
+  } = {}
+) => {
+  if (
+    typeof window === "undefined" ||
+    typeof map !== "object" ||
+    (!preventScroll && !preventTouch) ||
+    typeof textScroll !== "string" ||
+    typeof textTouch !== "string"
+  )
+    return;
 
-  function createEl(appendBlock, attributes = {}, tag = 'div') {
-    const el = document.createElement(tag);
-    for (const attribute of Object.keys(attributes)) el[attribute] = attributes[attribute];
-    appendBlock.appendChild(el);
-    return el;
+  const isTouch =
+    /Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent);
+
+  const eventsPane = map.panes.get("events");
+  const eventsPaneEl = eventsPane.getElement();
+
+  const text = isTouch ? textTouch : textScroll;
+
+  const styles = {
+    alignItems: "center",
+    boxSizing: "border-box",
+    color: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    padding: "20px",
+    textAlign: "center",
+    transition: "background .2s",
+    touchAction: "auto"
+  };
+
+  Object.keys(styles).forEach(name => {
+    eventsPaneEl.style[name] = styles[name];
+  });
+
+  const hintToggle = fl => {
+    eventsPaneEl.style.background = `rgba(0, 0, 0, ${fl ? ".6" : "0"})`;
+    eventsPaneEl.textContent = fl ? text : "";
+  };
+
+  if (preventTouch && isTouch) {
+    map.behaviors.disable("drag");
+
+    ymaps.domEvent.manager.add(eventsPaneEl, "touchmove", e => {
+      hintToggle(e.get("touches").length === 1);
+    });
+
+    ymaps.domEvent.manager.add(eventsPaneEl, "touchend", () => {
+      hintToggle(false);
+    });
   }
 
-  const parentEl = map.container.getParentElement();
-  const mapEl = map.container.getElement();
-  const isMobile = /Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent);
-  let isBlockShow = false;
+  if (preventScroll && !isTouch) {
+    const scrollToggle = fl => {
+      const behavior = "scrollZoom";
+      fl ? map.behaviors.enable(behavior) : map.behaviors.disable(behavior);
+    };
 
-  if (getComputedStyle(parentEl).position === 'static') parentEl.style.position = 'relative';
-
-  const css = '.ymaps-touch-scroll{position:absolute;top:0;left:0;transition:opacity .2s;width:100%;height:100%;overflow:hidden;z-index:-2147483648}' +
-    '.ymaps-touch-scroll:before{content:"";display:inline-block;vertical-align:middle;height:100%}' +
-    '.ymaps-touch-scroll-bg{position:absolute;top:0;left:0;background:#000;height:100%;width:100%}' +
-    '.ymaps-touch-scroll-content{position:relative;display:inline-block;vertical-align:middle;width:100%;color:#fff;text-align:center;box-sizing:border-box}';
-  const style = createEl(document.head, {type: 'text/css'}, 'style');
-  style.appendChild(document.createTextNode(css));
-
-  const block = createEl(parentEl, {className: 'ymaps-touch-scroll'});
-  const bg = createEl(block, {className: 'ymaps-touch-scroll-bg'});
-  const content = createEl(block, {className: 'ymaps-touch-scroll-content'});
-
-  mapEl.style.transition = 'opacity .2s';
-  const margin = map.margin.getMargin();
-  for (const i in margin) margin[i] += 20;
-  content.style.padding = margin.join('px ') + 'px';
-  content.textContent = isMobile ? textTouch : textScroll;
-
-  function blockToggle(show = true) {
-    if ((show && isBlockShow) || (!show && !isBlockShow)) return;
-    isBlockShow = show;
-
-    block.style.opacity = show ? '1' : '0';
-    mapEl.style.opacity = show ? '.3' : '1';
-  }
-
-  function dragToggle(on = true) {
-    on ? map.behaviors.enable('drag') : map.behaviors.disable('drag');
-  }
-  
-  if (preventScroll && !isMobile) {
+    let isMouseEnter = false;
     let isCtrlPress = false;
-    let isScrollOn = true;
-
-    document.addEventListener('keydown', e => {
-      isCtrlPress = e.keyCode === 17;
-      if (isCtrlPress) blockToggle(false);
-    });
-
-    document.addEventListener('keyup', e => {
-      if (e.keyCode === 17) isCtrlPress = false;
-    });
-
-    function scrollToggle(on = true) {
-      if ((on && isScrollOn) || (!on && !isScrollOn)) return;
-      isScrollOn = on;
-      on ? map.behaviors.enable('scrollZoom') : map.behaviors.disable('scrollZoom');
-    }
 
     scrollToggle(false);
 
-    map.events.add('wheel', () => {
+    eventsPane.events.add("wheel", () => {
+      if (!isMouseEnter) return;
+
       scrollToggle(isCtrlPress);
-      dragToggle(isCtrlPress);
-      blockToggle(!isCtrlPress);
+      hintToggle(!isCtrlPress);
     });
 
-    map.events.add('mouseleave', () => {
-      blockToggle(false);
-      dragToggle(true);
+    eventsPane.events.add("mouseenter", () => {
+      isMouseEnter = true;
     });
 
-    map.events.add('mousedown', () => {
-      blockToggle(false);
-      dragToggle(true);
-    });
-  }
-
-  if (preventTouch && isMobile) {
-    dragToggle(false);
-
-    ymaps.domEvent.manager.add(mapEl, 'touchmove', e => {
-      const twoFingers = e.get('touches').length === 2;
-      blockToggle(!twoFingers);
-      dragToggle(twoFingers);
+    eventsPane.events.add("mouseleave", () => {
+      isMouseEnter = false;
+      hintToggle(false);
     });
 
-    ymaps.domEvent.manager.add(mapEl, 'touchend', () => {
-      blockToggle(false);
+    document.addEventListener("keydown", e => {
+      isCtrlPress = e.ctrlKey;
+      if (isCtrlPress) hintToggle(false);
+    });
+
+    document.addEventListener("keyup", () => {
+      isCtrlPress = false;
     });
   }
-}
+};
+
+export default ymapsTouchScroll;
